5f26c4f6f4e3144917666848570ef49d54c8890a
Murli Khaire <115461606+Murli0399@users.noreply.github.com>
2023-06-30 17:54:25 +0530

    Update Problem8.py

---
+++ Set-03/Problem8.py
@@ -0,0 +1,12 @@
+def count_words(input_file, output_file):
+    with open(input_file, 'r') as file:
+        text = file.read()
+        word_count = len(text.split())
+
+    with open(output_file, 'w') as file:
+        file.write(f"Number of words: {word_count}")
+
+input_file = "input.txt"
+output_file = "output.txt"
+
+count_words(input_file, output_file)
